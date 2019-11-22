50016ed48bab20b1606ce0043d1785854f85f8fa
eagamer <eagamer.060@gmail.com>
2019-11-22 18:31:43 +0600

    fixing utf-8

--- js/registration-ctrl.js
+++ js/registration-ctrl.js
@@ -112,7 +112,7 @@ app.controller("registration", function($scope, $http,$location,$window, $timeou
             console.log(data);
             console.log('specValues:' , values);
             $timeout(function () {
-                // $window.location.href = 'thanks.html';
+                $window.location.href = 'thanks.html';
             }, 120);
             // var insertedId = parseInt(data.data.replace(' ',''));
             // console.log('crassavchik' + $scope.user.email);
